86583a443cc96ed46040407a5436097e50b25947
huanb <huanb@synnex.com>
2021-10-26 01:52:19 -0300

    Ajuste no Index para verificar saude da API

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const ambeeAPI = "https://api.ambeedata.com/";
-const API_KEY = process.env.ambee_API_KEY;
-
-const headers = {
-    "content-type": "application/json",
-    "x-api-key": API_KEY,
-    Host: "api.ambeedata.com",
-  };
-
-router.get("/", (req, res, next) => {
-  console.log('teste');
-
-  fetch(`${ambeeAPI}/latest/fire?lat=-23.537205&lng=-46.796230`, {
-    method: "GET",
-    headers: headers,
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-
+router.get('/', (req, res, next) => {
+  res.status(200).send({
+    title: 'Clima e Cuidados API',
+    version: '1.0.0'
+  });
 });
 
 module.exports = router;
